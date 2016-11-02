3bb7b3a47c9900a17eb3a6e262d32307602eb133
Jaclyn Feminella <jfeminella@xogrp.com>
2016-11-02 11:14:11 -0400

    Add Wedding Album view as index2

---
+++ js/app/ParallaxCat.js
@@ -0,0 +1,63 @@
+define(function(require, exports, module) {
+    var View = require('samsara/core/View');
+    var Transform = require('samsara/core/Transform');
+    var Surface = require('samsara/dom/Surface');
+    var ContainerSurface = require('samsara/dom/ContainerSurface');
+
+    // ParallaxCat is a single image in the ParallaxCats scrollview
+    // It listens to the scrollview's progress and translates accordingly
+    // to create a parallax effect
+    var ParallaxCat = View.extend({
+        defaults : {
+            src : '',
+            parallaxAmount : 70,
+            index : 0,
+            skew : 0
+        },
+        initialize: function (options) {
+            // Each cat image is placed inside a container with
+            // `overflow : hidden` to clip the content
+            var container = new ContainerSurface({
+                classes: ['cat'],
+                properties: {overflow: 'hidden'}
+            });
+
+            // Create the cat photo
+            var cat = new Surface({
+                tagName : 'img',
+                origin : [0.5,0.2],
+                proportions : [1, false],
+                aspectRatio : 4/3,
+                attributes : {src : options.src}
+            });
+
+            // Transform the image inside of the container by
+            // mapping the input (provided by the scrollview) to a translation
+            var parallaxTransform = this.input.map(function (data) {
+                var offset = options.parallaxAmount * (data.index + data.progress);
+                return Transform.translateY(offset)
+            });
+
+            // Build the render subtree inside the container
+            container
+                .add({
+                    align : [.5, 0.2],
+                    transform: Transform.compose(
+                        Transform.translateY(-options.index * options.parallaxAmount),
+                        Transform.skewY(options.skew)
+                    )
+                })
+                .add({
+                    transform: parallaxTransform
+                })
+                .add(cat);
+
+            // Build the render subtree for the view
+            this
+                .add({transform: Transform.skewY(-options.skew)})
+                .add(container);
+        }
+    });
+
+    module.exports = ParallaxCat;
+});
---
+++ js/app/ParallaxCats.js
@@ -0,0 +1,49 @@
+define(function (require, exports, module) {
+    var View = require('samsara/core/View');
+    var Surface = require('samsara/dom/Surface');
+    var Scrollview = require('samsara/layouts/Scrollview');
+    var ParallaxCat = require('./ParallaxCat');
+
+    // ParallaxCats is a scrollview of ParallaxCat images
+    var ParallaxCats = View.extend({
+        defaults: {
+            skew : 0,
+            parallaxAmount : 0,
+            urls : []
+        },
+        initialize: function (options) {
+            // Create the scrollview
+            var scrollview = new Scrollview({
+                direction: Scrollview.DIRECTION.Y,
+                drag: 0.3
+            });
+
+            // Create the parallaxCat images
+            var cats = [];
+            for (var i = 0; i < options.urls.length; i++) {
+                var parallaxCat = new ParallaxCat({
+                    proportions : [1, 1/2],
+                    skew: options.skew,
+                    src: options.urls[i],
+                    parallaxAmount: options.parallaxAmount,
+                    index: i
+                });
+
+                // Subscribe each image to the scrollview
+                // so that they can shift themselves based
+                // on the scrollview's progress to create a
+                // parallax effect
+                parallaxCat.subscribe(scrollview);
+
+                cats.push(parallaxCat);
+            }
+
+            scrollview.addItems(cats);
+
+            // Build the render subtree consisting of only the scrollview
+            this.add(scrollview);
+        }
+    });
+
+    module.exports = ParallaxCats;
+});
